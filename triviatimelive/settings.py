e5f2390de13753e75cabfbbc90b3a915653c3e53
Miles Edland <miles.edland@gmail.com>
2018-03-27 21:00:24 +0000

    basic changes to get it working on production

--- triviatimelive/settings.py
+++ triviatimelive/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-j)7db3h968d3=5+_pl+2o@5rb=2jdcu*j96hb1y6rn!gg$5cu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shutupmiles.webfactional.com']
 
 
 # Application definition
@@ -119,4 +119,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = '/home/shutupmiles/webapps/triviatimelive/ttl-website/static'
 STATIC_URL = '/static/'
