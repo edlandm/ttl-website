250e0e9d04d54c436f900f672fddb5ec72d60e41
Miles Edland <miles.edland@gmail.com>
2018-04-11 15:45:37 -0700

    Add VenueDiscount model and use it to populate the table in venue_discounts.html

--- palooza/admin.py
+++ palooza/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Player, CheckIn
+from .models import Player, CheckIn, VenueDiscount
 
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ("player_id", "name", "points")
+    list_display = ("player_id", "name", "points", "phone")
     list_filter = ["name", "date_added"]
 
     def player_id(self, obj):
@@ -15,5 +15,10 @@ class CheckInAdmin(admin.ModelAdmin):
     list_display = ("date", "player", "venue")
     list_filter = ["venue", "date", "player"]
 
+
+class VenueDiscountAdmin(admin.ModelAdmin):
+    list_display = ("venue", "discount")
+
 admin.site.register(Player, PlayerAdmin)
 admin.site.register(CheckIn, CheckInAdmin)
+admin.site.register(VenueDiscount, VenueDiscountAdmin)
