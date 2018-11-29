d1a78dfa396e32df0040528d9ced05d936ee291f
Miles Edland <miles.edland@gmail.com>
2018-11-28 19:04:56 -0800

    add logo field to Venue model

--- triviatimelive/urls.py
+++ triviatimelive/urls.py
@@ -17,6 +17,9 @@ from django.conf    import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls    import include, path
+from django.conf    import settings
+from django.conf.urls.static import static
+
 
 app_name = 'triviatimelive'
 urlpatterns = [
@@ -24,3 +27,6 @@ urlpatterns = [
     path('triviatimelive/triviapalooza/', include('palooza.urls')),
     path('triviatimelive/', include('website.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
